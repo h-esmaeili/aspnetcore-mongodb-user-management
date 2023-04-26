8550e3a5c5d19f5dea163810cd98e29ecb4a1bff
hamid <hamid@abacus.co>
2023-04-26 16:15:53 +0330

    Created backend project and test project

---
+++ src/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
